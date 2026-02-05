9999aa0338822141039eb318bbc3323a4fa47acc
Veetrag Patil <156281840+patilvp24@users.noreply.github.com>
2026-02-05 16:30:13 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,6 +1,8 @@
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-         System.out.println("Done with task2");
+        System.out.println("Done with task2");
+        System.out.println("Auto build triggered!");
+
     }
 }
